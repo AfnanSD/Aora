89b01646b137f06c27fa00a504db0adc86725119
AfnanSD <99503463+AfnanSD@users.noreply.github.com>
2024-06-13 08:22:22 +0300

    finish welcome screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,16 @@
-import { Link } from 'expo-router';
+import { Redirect, router } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { ScrollView, Text, View, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../constants';
 import CustomButton from '../components/customButton';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
     return (
         <SafeAreaView className="bg-primary h-full">
             <ScrollView contentContainerStyle={{ height: "100%" }}>
-                <View className="w-full justify-center items-center h-full px-4">
+                <View className="w-full justify-center items-center min-h-[85vh] px-4">
                     <Image
                         source={images.logo}
                         className="w-[130px] h-[84]px"
@@ -34,10 +35,16 @@ export default function App() {
 
                     <Text className="text-sm text-gray-100 mt-7 text-center">Where creativity meets innovation: embark on a journy of limitless exploration with Aora</Text>
 
-                    <CustomButton />
+                    <CustomButton 
+                    title="Continue with Email"
+                    handlePress={()=>router.push('/sign-in')}
+                    containerStyle="w-full mt-7"
+                    />
 
                 </View>
             </ScrollView>
+
+            <StatusBar backgroundColor='#161622'  style='light'/>
         </SafeAreaView>
     );
 }
--- components/customButton.jsx
+++ components/customButton.jsx
@@ -1,11 +1,16 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 
-const CustomButton = () => {
+const CustomButton = ({ title, handlePress, containerStyle, textStyle, isLoading }) => {
   return (
-    <View>
-      <Text>CustomButton</Text>
-    </View>
+    <TouchableOpacity
+      className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyle} ${isLoading ? 'opacity-50' : ''}`}
+      onPress={handlePress}
+      activeOpacity={0.7}
+      disabled={isLoading}
+    >
+      <Text className={`text-primary font-psemibold text-lg ${textStyle}`}>{title}</Text>
+    </TouchableOpacity>
   )
 }
 
